07c66ecc044453fcd6dda9cd386daf0fd57acc6e
pgurgel <programador.pg.2023@gmail.com>
2024-02-28 21:37:09 -0300

    Update breakContinue.js

--- session5/breakContinue.js
+++ session5/breakContinue.js
@@ -1,7 +1,7 @@
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for (x in nums) {
-    if(x == 5) {
+    if (x == 5) {
         break
     }
     console.log(`${x} = ${nums[x]}`)
@@ -16,7 +16,7 @@ for (y in nums) {
 
 externo: for (a in nums) {
     for (b in nums) {
-        if(a == 2 && b ==3) break externo
+        if (a == 2 && b == 3) break externo
         console.log(`Par = ${a},${b}`)
     }
 }
